8952ef3e92b9cc8d500771fbde39c6f46a4d25b3
Paul Alesius <paul@unnservice.com>
2023-09-03 22:25:27 +0200

    Configured themes and modeline

--- init.el
+++ init.el
@@ -1,1 +1,13 @@
 (straight-use-package 'use-package)
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
