0856300cbec53192cea63576b3394aee1f81ee8e
Paul Alesius <paul@unnservice.com>
2023-09-04 00:28:05 +0200

    Add orderless

--- init.el
+++ init.el
@@ -73,4 +73,16 @@
 
 (use-package vertico
   :init
-  (vertico-mode))
+  (vertico-mode)
+  (require 'vertico-mouse)
+  (vertico-mouse-mode))
+;;(use-package vertico-mouse
+;;  :after vertico
+;;  :config
+;;  (vertico-mouse-mode))
+
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
