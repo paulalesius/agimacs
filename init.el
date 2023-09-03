9db4ea6d903ca98d2fd518fe54560aebade63056
Paul Alesius <paul@unnservice.com>
2023-09-03 23:34:37 +0200

    Add evil and which-key

--- init.el
+++ init.el
@@ -1,17 +1,46 @@
 (straight-use-package 'use-package)
 
 (use-package doom-themes
-  :config
+  :init
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
+  :config
   (load-theme 'doom-one t)
   (doom-themes-visual-bell-config)
   (doom-themes-org-config))
 
-(use-package doom-modeline
-  :ensure t
+(use-package evil
+  :init
+  (setq evil-want-integration t
+        evil-want-keybinding nil)
   :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :custom
+  (evil-collection-setup-minibuffer t)
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package projectile
+  :config
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
+(use-package doom-modeline
+  :init
   (setq doom-modeline-project-detection 'projectile
         doom-modeline-buffer-file-name-style 'truncate-upto-project)
+  :config
   :hook (after-init . doom-modeline-mode))
-(message "Hello")
+
+(use-package which-key
+  :after evil
+  :init
+  (setq which-key-idle-delay 0.4)
+  :config
+  (which-key-mode)
+  (which-key-setup-minibuffer))
